7ad409682a23a99878b93555116f9730e7a6e245
Jani Lähdesmäki <janilabo@gmail.com>
2013-08-05 00:19:35 +0300

    0.29

--- src/string/line.pas
+++ src/string/line.pas
@@ -6,56 +6,19 @@
 
 function StrLine(str: string; number: Integer): string; callconv
 var
-  p, h, i, x, m, l, y, z: Integer;
-  d: TStringArray;
+  d, r: TStringArray;
 begin
   if ((str <> '') and (number > 0)) then
   begin
-    i := 0;
     SetLength(d, 3);
     d[0] := #13#10;
     d[1] := #13;
     d[2] := #10;
-    h := High(d);
-    repeat
-      l := 0;
-      x := 0;
-      while (x <= h) do
-      begin
-        p := Pos(d[x], str);
-        if (p < 1) then
-        begin
-          z := High(d);
-          if ((x <= z) and (x > -1)) then
-          begin
-            for y := x to (z - 1) do
-              d[y] := d[(y + 1)];
-            SetLength(d, z);
-          end;
-          Dec(x);
-          Dec(h);
-        end else
-          if ((l = 0) or (p < l)) then
-          begin
-            m := x;
-            l := p;
-          end;
-        Inc(x);
-      end;
-      if (l > 0) then
-      begin
-        Inc(i);
-        if (i >= number) then
-          Result := Copy(str, 1, (l - 1))
-        else
-          Delete(str, 1, ((l + Length(d[m])) - 1));
-      end else
-        if ((i + 1) >= number) then
-          Result := Copy(str, 1, Length(str))
-        else
-          Result := '';
-    until ((i > number) or (l < 1));
-    SetLength(d, 0);
+    StrExplodeMultiEx(d, str, (number + 1), r);
+    if (Length(r) >= number) then
+      Result := r[(number - 1)]
+    else
+      Result := '';
   end else
     Result := '';
 end;
@@ -68,52 +31,15 @@ end;
 
 procedure StrLines(str: string; var output: TStringArray); callconv
 var
-  p, h, i, x, m, l, y, z: Integer;
   d: TStringArray;
 begin
   if (str <> '') then
   begin
-    i := 0;
     SetLength(d, 3);
     d[0] := #13#10;
     d[1] := #13;
     d[2] := #10;
-    h := High(d);
-    SetLength(output, Length(str));
-    repeat
-      l := 0;
-      x := 0;
-      while (x <= h) do
-      begin
-        p := Pos(d[x], str);
-        if (p < 1) then
-        begin
-          z := High(d);
-          if ((x <= z) and (x > -1)) then
-          begin
-            for y := x to (z - 1) do
-              d[y] := d[(y + 1)];
-            SetLength(d, z);
-          end;
-          Dec(x);
-          Dec(h);
-        end else
-          if ((l = 0) or (p < l)) then
-          begin
-            m := x;
-            l := p;
-          end;
-        Inc(x);
-      end;
-      if (l > 0) then
-      begin
-        output[i] := Copy(str, 1, (l - 1));
-        Delete(str, 1, ((l + Length(d[m])) - 1));
-        Inc(i);
-      end else
-        output[i] := Copy(str, 1, Length(str));
-    until (l = 0);
-    SetLength(output, (i + 1));
+    StrExplodeMulti(d, str, output);
   end else
   begin
     SetLength(output, 1);
@@ -181,49 +107,14 @@ end;
 
 function StrLineCount(str: string): Integer; callconv
 var
-  h, l, p, o, i, t, x, y: Integer;
   s: TStringArray;
 begin
-  Result := 1;
   if (str <> '') then
   begin
-    l := 0;
-    SetLength(s, 3);
-    s[0] := #13#10;
-    s[1] := #13;
-    s[2] := #10;
-    h := High(s);
-    o := 1;
-    repeat
-      p := 0;
-      x := 0;
-      while (x <= h) do
-      begin
-        t := StrPosEx(s[x], str, (l + o));
-        if (t < 1) then
-        begin
-          for y := x to (h - 1) do
-            s[y] := s[(y + 1)];
-          SetLength(s, h);
-          Dec(x);
-          Dec(h);
-        end else
-          if ((p = 0) or (t < p)) then
-          begin
-            p := t;
-            i := x;
-          end;
-        Inc(x);
-      end;
-      if (p > 0) then
-      begin
-        Inc(Result);
-        l := p;
-        o := Length(s[i]);
-      end;
-    until (p <= 0);
-    SetLength(s, 0);
-  end;
+    StrLines(str, s);
+    Result := Length(s);
+  end else
+    Result := 1;
 end;
 
 {==============================================================================]
@@ -234,24 +125,20 @@ end;
 
 function StrLineByPos(str: string; position: Integer): Integer; callconv
 var
-  s: TStringArray;
-  i, n, o: Integer;
+  c, l: Integer;
 begin
   if (position > 0) then
   begin
-    Result := 1;
-    str := Copy(str, 1, position);
-    SetLength(s, 3);
-    s[0] := #13#10;
-    s[1] := #13;
-    s[2] := #10;
-    for i := 0 to 2 do
+    l := Length(str);
+    if (l > 0) then
     begin
-      o := Length(str);
-      str := StringReplace(str, s[i], '', [rfReplaceAll]);
-      n := ((o - Length(str)) div Length(s[i]));
-      Result := (Result + n);
-    end;
+      if (position > l) then
+        position := l;
+      str := Copy(str, 1, position);
+      c := StrCount(#13#10, str);
+      Result := (c + (StrCount(#10, str) - c) + (StrCount(#13, str) - c) + 1);
+    end else
+      Result := 1;
   end else
     Result := 0;
 end;
